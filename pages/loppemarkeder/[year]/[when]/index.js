5ffc3271ae21eae87a629d4f47e0b0cc7d261507
Jonas Knutsen <jonas@j10n.no>
2022-10-18 20:07:56 +0200

    bugfix

--- pages/loppemarkeder/[year]/[when]/index.js
+++ pages/loppemarkeder/[year]/[when]/index.js
@@ -35,7 +35,7 @@ export default function Home ({ data, year, when, places, organizers }) {
       {!isNaN(when) && <Typography variant='h4' component='h1' align='center' gutterBottom>Loppemarkeder uke {when} {year}</Typography>}
       {!isNaN(when) && <Typography variant='h5' component='h2' align='center' gutterBottom>I uke {when} har vi registrert {events.length} loppemarkeder</Typography>}
       <Stack spacing={2}>
-        {!isNaN(when) && <MapCard places={mapArray} />}
+        {!isNaN(when) && events.length > 0 && <MapCard places={mapArray} />}
         {events?.map((event, key) => {
           return (
             <EventCard key={key} event={event} />
